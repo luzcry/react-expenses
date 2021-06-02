311640e950b72c87d394528cc236d1c04f02dd40
luzcry <mcrodriguezt618@gmail.com>
2021-06-01 23:46:17 -0500

    using Map

--- src/components/ExpenseContainer.js
+++ src/components/ExpenseContainer.js
@@ -2,29 +2,17 @@ import { Fragment } from 'react';
 import ExpenseItem from './ExpenseItem';
 
 function ExpenseContainer({expenses}) {
+  const loop = expenses.map((expense) =>
+  <ExpenseItem
+    title={expense.title}
+    amount={expense.amount}
+    date={expense.date}
+  />
+  )
   return (
     <Fragment>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
-      </Fragment>
+     {loop}
+    </Fragment>
         );
 }
 
