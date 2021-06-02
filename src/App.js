24121c0d753e8d96e096f2c1c213ebd6f12a49e8
luzcry <mcrodriguezt618@gmail.com>
2021-06-01 23:27:35 -0500

    Practice Making new coMponent

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import ExpenseItem from './components/ExpenseItem';
+import ExpenseContainer from './components/ExpenseContainer';
+import './components/Expenses.css';
 
-function App() {
+function App(props) {
   const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
     {
       id: 'e1',
       title: 'Toilet Paper',
@@ -26,26 +33,11 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
       <p>Hi</p>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <div className="expenses">
+        <ExpenseContainer
+          expenses={expenses}
+        />
+      </div>
     </div>
   );
 }
